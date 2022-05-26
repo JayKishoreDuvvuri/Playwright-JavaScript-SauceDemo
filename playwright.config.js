cfe2e627d10d24cc368bc398143bfcd45b0e0123
Jay Kishore Duvvuri <jaykishore96@ymail.com>
2022-05-26 16:31:55 +0300

    Headless Run on Firefox

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testDir: "tests",
   timeout: 60000,
   retries: 1,
-  reporter: [["line"]],
+  reporter: [["line"], ["./CustomReporter.js"]],
   projects: [
     {
       name: `Chrome`,
