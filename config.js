b542334ef901a53652ae88f2f3e8228525096326
JayKishoreDuvvuri <jaykishore999@gmail.com>
2022-12-31 15:27:14 +0200

    Added export(ES6 syntax) statements to pageobjects from module.exports

--- config.js
+++ config.js
@@ -1,24 +1,20 @@
-module.exports = {
-	baseUrl: 'https://www.saucedemo.com/',
-	landingPageUrl: 'https://www.saucedemo.com/inventory.html',
-	title: 'Swag Labs',
-	sauceLabsTitle:
-		'Cross Browser Testing, Selenium Testing, Mobile Testing | Sauce Labs',
-	sauceLabsUrl: 'https://saucelabs.com/',
-	onesie: '$7.99',
-	fleeceJacket: '$49.99',
-	tShirtRed: '$15.99',
-	backPack: '$29.99',
-	twitterTitle: 'Twitter',
-	twitterUrl: 'https://twitter.com/saucelabs',
-	facebookTitle: 'Sauce Labs | Facebook',
-	facebookUrl: 'https://www.facebook.com/saucelabs',
-	linkedInTitle: 'LinkedIn',
-	linkedInUrl: 'https://www.linkedin.com',
-	yourCartUrl: 'https://www.saucedemo.com/cart.html',
-	checkoutYourInformationUrl:
-		'https://www.saucedemo.com/checkout-step-one.html',
-	checkoutOverviewUrl: 'https://www.saucedemo.com/checkout-step-two.html',
-	checkoutCompleteUrl: 'https://www.saucedemo.com/checkout-complete.html',
-	productDetailsPageUrl: 'https://www.saucedemo.com/inventory-item.html?id=4'
-}
+export const baseUrl = 'https://www.saucedemo.com/';
+export const landingPageUrl = 'https://www.saucedemo.com/inventory.html';
+export const title = 'Swag Labs';
+export const sauceLabsTitle = 'Cross Browser Testing, Selenium Testing, Mobile Testing | Sauce Labs';
+export const sauceLabsUrl = 'https://saucelabs.com/';
+export const onesie = '$7.99';
+export const fleeceJacket = '$49.99';
+export const tShirtRed = '$15.99';
+export const backPack = '$29.99';
+export const twitterTitle = 'Twitter';
+export const twitterUrl = 'https://twitter.com/saucelabs';
+export const facebookTitle = 'Sauce Labs | Facebook';
+export const facebookUrl = 'https://www.facebook.com/saucelabs';
+export const linkedInTitle = 'LinkedIn';
+export const linkedInUrl = 'https://www.linkedin.com';
+export const yourCartUrl = 'https://www.saucedemo.com/cart.html';
+export const checkoutYourInformationUrl = 'https://www.saucedemo.com/checkout-step-one.html';
+export const checkoutOverviewUrl = 'https://www.saucedemo.com/checkout-step-two.html';
+export const checkoutCompleteUrl = 'https://www.saucedemo.com/checkout-complete.html';
+export const productDetailsPageUrl = 'https://www.saucedemo.com/inventory-item.html?id=4';
