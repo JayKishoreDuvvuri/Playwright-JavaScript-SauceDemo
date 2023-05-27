c18e8f3bc58119773db8702140aff2b09098841a
Jay Kishore Duvvuri <jaykishore96@ymail.com>
2023-05-27 18:56:09 +0300

    Allure Test Report

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ export const projects = [
 		use: {
 			browserName: `chromium`,
 			channel: `chrome`,
-			headless: false,
+			headless: true,
 			viewport: { width: 1720, height: 850 },
 			screenshot: `only-on-failure`,
 			video: `retain-on-failure`,
